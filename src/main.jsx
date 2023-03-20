2301bb6abf05fd873c103df170f2636a08ec9904
Henry Bautista <henry.bautista.prado@icloud.com>
2023-03-19 19:55:38 -0600

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,19 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
+/* components */
+import Paths from './router'
+/* styles */
+import 'bootstrap/dist/css/bootstrap.css'
+import './styles/index.css'
+
+const root = createRoot(document.getElementById('root'))
+
+root.render(
+  <StrictMode>
+    {/* exponer las rutas en mi aplicación */}
+    <BrowserRouter>
+      <Paths />
+    </BrowserRouter>
+  </StrictMode>
+)
